e5f0d305f0f2ac11004f9638b1a52eb568d8a33b
momo <momoko.nakada1002@gmail.com>
2021-07-05 00:01:25 -0700

    work radios / keep radio value

--- flash.js
+++ flash.js
@@ -1,52 +1,110 @@
 const input  = document.querySelector('input[name="answer-input"]');
-let   inputValue;
 const button = document.querySelector('button');
 const result = document.querySelector('#result');
-const radioDigit = document.querySelector('input[name=radio-digit]:checked');
-const radioTime  = document.querySelector('input[name=radio-time]:checked');
-const radioDigitValue = parseInt(radioDigit.value);
-const radioTimeValue  = parseFloat(radioTime.value);
+const formElementById = document.getElementById('setting-form');
+const randomNumberDisplay = document.querySelector('#number-display');
+const randomNumberDisplayClass = document.querySelector('.number-flash-container__display');
+const radio = document.querySelectorAll('.radio');
 
-const randomNumberDisplay = document.querySelector('#container__number-flash');
+const radioDigit = document.getElementsByName("radio-digit");
+const radioTime = document.getElementsByName("radio-time");
+const radioColor = document.getElementsByName("radio-color");
+
+let radioDigitValue = 5;
+let radioTimeValue = 500;
+let radioColorValue = 'white';
+
 
 let randomNumber;
 let textLength;
-let count = 0;
+
+window.addEventListener( "DOMContentLoaded" , ()=> {
+    radioDigit.forEach(
+        r => r.addEventListener("change" ,
+                 e => {
+                    radioDigitValue = e.target.value;
+                    result.innerHTML = radioDigitValue + "ケタ入力すると結果がでます。";
+                 }
+            )
+    );
+
+
+    radioTime.forEach(
+        r => r.addEventListener("change" ,
+                 e => radioTimeValue = e.target.value
+            )
+    );
+
+
+    radioColor.forEach(
+        r => r.addEventListener("change" ,
+                 e => {
+                    radioColorValue = e.target.value;
+                    randomNumberDisplayClass.style.color = radioColorValue;
+                 }
+            )
+    );
+
+});
 
 window.onload = () => {
     input.focus();
 }
 
-try {
-    button.onclick = showRandomNumber(radioDigitValue);
-    // input.onkeyup = inputCheck();
-} catch (e) {
-    console.error(e);
-}
+let form = document.querySelector('#setting-form');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    input.value = "";
+
+    radioDigitValue  = getSetting('radio-digit');
+    radioTimeValue = getSetting('radio-time');
+    radioColorValue = getSetting('radio-color');
+    new Promise((resolve) => {
+        showRandomNumber(radioDigitValue);
+        resolve();
+    }).then(() => {
+        setDisplayNone(radioTimeValue);
+    });
+
+}, false);
+
 
 function setDisplayNone(){
     setTimeout(() => {
         randomNumberDisplay.textContent = '';
-    }, 500);
+    }, parseInt(radioTimeValue));
+}
+
+
+function getSetting(radioName){
+    let value;
+    let radios = formElementById.elements[radioName];
+
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            value = radios[i].value; 
+        }
+    }
+    return value;
 }
 
 function showRandomNumber(v){
     let minNumber = 0;
     let maxNumber = 0;
-    switch (v) {
+    switch (parseInt(v)) {
         case 5:
             minNumber = 10000;
-            maxNumber = 100000-1;
+            maxNumber = 99999;
             break;
     
         case 6:
             minNumber = 100000;
-            maxNumber = 1000000-1;
+            maxNumber = 999999;
             break;
     
         case 7:
             minNumber = 1000000;
-            maxNumber = 10000000-1;
+            maxNumber = 9999999;
             break;
     
         default:
@@ -54,16 +112,14 @@ function showRandomNumber(v){
     }
 
     randomNumber = Math.floor( Math.random() * (maxNumber - minNumber) + minNumber );
-    randomNumberDisplay.textContent = parseInt(randomNumber);
-
-    setDisplayNone();
+    randomNumberDisplay.innerHTML = randomNumber;
+    console.log(randomNumber);
 } 
 
 
 function inputCheck() {
-    inputValue = input.value;
+    let inputValue = input.value;
     textLength = inputValue.length;
-    console.log(radioDigitValue);
 
     if(textLength == radioDigitValue){
 
